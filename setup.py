ad4ef6de4beaf662c82dc1876d23155a3db15402
Sintax1 <sintax@obscurepacket.org>
2017-03-19 00:15:36 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dbusservice',
+      version='0.1',
+      description='Simple DBUS Service and Client',
+      url='https://github.com/sintax1/dbusservice',
+      author='sintax1',
+      author_email='sintax@obscurepacket.org',
+      license='MIT',
+      packages=['dbusservice'],
+      zip_safe=False)
